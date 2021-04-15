709957844da856ea452c9ebc0f0d8cfae8dfe407
avraam754 <74533385+avraam754@users.noreply.github.com>
2021-04-15 10:09:54 +0300

    удалили функцию
    Для демонстрации обновления

--- testGit/Program.cs
+++ testGit/Program.cs
@@ -8,10 +8,7 @@ namespace testGit
 {
     class Program
     {
-        static void print()
-        {
-            Console.WriteLine("Это функция");
-        }
+      
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
